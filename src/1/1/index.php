a95ae117399d76a3920169cf84f46bc210bc586f
Jeremy Ward <jeremy@jmichaelward.com>
2021-12-04 10:04:28 -0600

    Solve problem 1.1.

--- src/1/1/index.php
+++ src/1/1/index.php
@@ -1,3 +1,19 @@
 <?php
 
 echo 'Day 1: Sonar Sweep' . PHP_EOL;
+
+$measurements = inputs_1_1();
+
+$increases = array_filter(
+	$measurements,
+	function( $measurement, $index ) use ( $measurements ) {
+		if ( ! isset( $measurements[ $index - 1 ] ) ) {
+			return false;
+		}
+
+		return $measurement > $measurements[$index - 1];
+	},
+	ARRAY_FILTER_USE_BOTH
+);
+
+echo count( $increases ) . PHP_EOL;
